852a7df8e880451b6c7329193bed5110bb22a230
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-03-16 22:07:27 -0300

    Add aggregation function.

--- src/containers/CategoryResultContainer.js
+++ src/containers/CategoryResultContainer.js
@@ -1,10 +1,28 @@
 import { connect } from 'react-redux'
+import sumBy from 'lodash/sumBy'
 
 import CategoryResult from './../components/CategoryResult'
 
+const scale = function(score, limit) {
+		const min = -limit;
+		const scaledValue = (score - min) / (limit - min);
+		const finalValue = -2 + (scaledValue * 4);
+		return parseFloat(Math.round(finalValue * 100) / 100).toFixed(2);
+}
+
+const aggregate = (categories, questions) => {
+  return categories.map(category => {
+    const categoryQuestions = questions.filter(q => q.category === category.label);
+    const scores = sumBy(categoryQuestions, p => p.value);
+    const value = scale(scores, categoryQuestions.length);
+    
+    return {...category, value: value}
+  })
+}
+
 const mapStateToProps = (state) => {
   return {
-    categories: state.categories
+    categories: aggregate(state.categories, state.questions)
   }
 }
 

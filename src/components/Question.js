cf8b1ac6cdaa93aee221c6715763936fa3b23f2a
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-03-16 21:06:17 -0300

    Add question result constants.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,34 @@
 import React, { PropTypes } from 'react';
 
+import QuestionResults from './../constants/QuestionResults'
+
+import './Question.css';
+
 const Question = ({ onValueChanged, label, value }) => {
   const isActive = expected => {
-    let className = "btn btn-default";
+    let className = 'btn btn-default';
      if (value === expected) {
-       className += " active";
+       className += ' active';
      }
      return className;
   };
 
-  return <div>
-      { label }
-
-      <div className="btn-group pull-right" data-toggle="buttons">
-        <button type="button" className={ isActive(-1) } onClick={() => onValueChanged(-1) }>
-          <span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
-        </button> 
-        <button type="button" className={ isActive(0) } onClick={() => onValueChanged(0 ) }>
-          <span className="glyphicon glyphicon-record" aria-hidden="true"></span>
-        </button> 
-        <button type="button" className={ isActive(1) } onClick={() => onValueChanged(+1) }>
-        <span className="glyphicon glyphicon-ok-circle" aria-hidden="true"></span>
-        </button> 
-      </div>
-
-      { value }
-    </div>;
+  return <div className='clearfix margin-bottom'>
+          <div className="pull-left margin-top">
+            { label }
+          </div>  
+          <div className='btn-group pull-right' data-toggle='buttons'>
+            <button type='button' className={ isActive(QuestionResults.Bad) } onClick={() => onValueChanged(QuestionResults.Bad) }>
+              <span className='glyphicon glyphicon-remove-circle' aria-hidden='true'></span>
+            </button> 
+            <button type='button' className={ isActive(QuestionResults.Mixed) } onClick={() => onValueChanged(QuestionResults.Mixed) }>
+              <span className='glyphicon glyphicon-record' aria-hidden='true'></span>
+            </button> 
+            <button type='button' className={ isActive(QuestionResults.Good) } onClick={() => onValueChanged(QuestionResults.Good) }>
+            <span className='glyphicon glyphicon-ok-circle' aria-hidden='true'></span>
+            </button> 
+          </div>
+        </div>;
 } 
 
 Question.propTypes = {
---
+++ src/constants/QuestionResults.js
@@ -0,0 +1,7 @@
+const QuestionResults = {
+    Bad: -1,
+    Mixed: 0,
+    Good: 1
+};
+
+export default QuestionResults

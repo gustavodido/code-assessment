90d8aa066862d76ff5e42af4bb11f5cb68630352
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-03-16 22:33:15 -0300

    Add developers tool support.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import App from './components/App';
 import Reducers from './reducers/Reducers'
@@ -9,7 +10,7 @@ import Reducers from './reducers/Reducers'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-let store = createStore(Reducers)
+let store = createStore(Reducers, devToolsEnhancer())
 
 ReactDOM.render(
    <Provider store={store}>
